5b4e7f74f6519dc418d4901be820dcf1dbdccf67
bernardow <bernardoritzel@gmail.com>
2023-02-14 16:38:30 -0300

    jQuery-fixed&animation-added

--- main.js
+++ main.js
@@ -1,4 +1,13 @@
-${document}.ready(function(){
-    console.log('Oi')
-})
+$(document).ready(function(){
+    $('header button').click(function(){
+        $('form').slideToggle();
+    })
 
+    $('#btnCancel').click(function(){
+        $('form').slideUp(600);
+    })
+
+    $('form').on('submit', function(e){
+        e.preventDefault();
+    })
+})
