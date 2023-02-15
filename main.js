6edf8ff099d83a14cfa81360d1d80fe812f304e9
bernardow <bernardoritzel@gmail.com>
2023-02-15 14:00:33 -0300

    final

--- main.js
+++ main.js
@@ -9,5 +9,14 @@ $(document).ready(function(){
 
     $('form').on('submit', function(e){
         e.preventDefault();
+        const enderecoImagemNova = $('#enderecoImagemNova').val();
+        const novoItem = $('<li style="display: none"></li>');
+        $(`<img src="${enderecoImagemNova}" />`).appendTo(novoItem);
+        $(`<div class="overlay-imgLink> 
+            <a target="_blank" title="Ver imagem em tamanho real" href="${enderecoImagemNova}" </a>
+        </div>`).appendTo(novoItem);
+        $(novoItem).appendTo('ul');
+        $(novoItem).fadeIn();
+        $('#enderecoImagemNova').val('');
     })
 })
