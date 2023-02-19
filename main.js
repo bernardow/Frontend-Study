9572731effe5683da0f7152e837aaa30176ac8f3
bernardow <bernardoritzel@gmail.com>
2023-02-19 20:27:09 -0300

    playlist-list-add

--- main.js
+++ main.js
@@ -1,4 +1,12 @@
 $(document).ready(function(){
+    const btnMenu = $('.menuHamburguer');
+
+    $(btnMenu).click(function(){
+        const sideBar = $('#sideMenu')
+        $(sideBar).slideToggle();
+
+    })
+
     $('.carousel').slick({
         autoplay: true,
         arrows: false,
